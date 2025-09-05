e1eeaadcd60c33a2ef3083bbcb91cd66071ebebc
LAPTOP-OD938IQ4\trucle <truc.lektmt0801@hcmut.edu.vn>
2025-09-05 17:33:07 +0700

    first commit

---
+++ CardGameScoringApp/card-games-scoring/src/navigation/AppNavigator.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "../screens/HomeScreen/HomeScreen";
+import GameScreen from "../screens/GameScreen/GameScreen";
+import { RootStackParamList } from "../types/navigation.types";
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+export default function AppNavigator() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Game" component={GameScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
---
+++ CardGameScoringApp/card-games-scoring/src/screens/GameScreen/GameScreen.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function GameScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Game Screen - Scoreboard here!</Text>
+    </View>
+  );
+}
